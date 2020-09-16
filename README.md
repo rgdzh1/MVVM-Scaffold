307550adf899f9b2c585148a9ce79e47f5d81ce8
Yey <rgdzh1@gmail.com>
2020-09-16 19:04:53 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MVVM-Scaffold
-MVVM脚手架
+自己封装的一个简单的脚手架,MVVM+Koin+KT协程
--- app/src/main/java/com/yey/plus/di/KoinModule.kt
+++ app/src/main/java/com/yey/plus/di/KoinModule.kt
@@ -1,12 +1,13 @@
 package com.yey.plus.di
 import org.koin.dsl.module
+
 val viewModelModule = module {
 //    viewModel { LoginVM(get()) }
 }
 
 val repositoryModule = module {
-    single { DefauleRetrofitClient.mBApi } // 常驻内存
-    single { ProgressRetrofitClient.mBApi }
+//    single { DefauleRetrofitClient.mBApi } // 常驻内存
+//    single { ProgressRetrofitClient.mBApi }
 //    factory { LoginRepo(get()) }
 }
 
